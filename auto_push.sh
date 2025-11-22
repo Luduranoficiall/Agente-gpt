77af8e80a1f34776523c8b6feb2609c9ffd1d08c
Seu Nome <luduranoficiall167@gmail.com>
2025-11-22 18:30:01 -0300

    Atualização automática 22/11/2025 18:30:01

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "/home/luduranoficiall/Área de trabalho/TRABALHOS DA EXTRAORDINARIA.AI/AGENTE GPT"
+cd "/home/luduranoficiall/Área de trabalho/TRABALHOS DA EXTRAORDINARIA.AI/AGENTE GPT" || exit 1
 git add .
 git commit -m "Atualização automática $(date '+%d/%m/%Y %H:%M:%S')" || exit 0
 git push origin main
